2b115864a1a16b3da40d27f994ab58c62ce892d7
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-07-27 04:27:28 +0900

    Initial commit

---
+++ src/main/java/com/example/barojwtsubject/BaroJwtSubjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.barojwtsubject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroJwtSubjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaroJwtSubjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barojwtsubject/BaroJwtSubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barojwtsubject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaroJwtSubjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
